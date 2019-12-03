8605afd60b641f24eec9dc92b851eba1b52e701a
Md. Mehedi Hasan Shawon <49952788+mehedi299@users.noreply.github.com>
2019-12-03 18:06:13 +0600

    Add files via upload

---
+++ Tic/src/Server1/Couter.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Server1;
+
+/**
+ *
+ * @author Student
+ */
+public class Couter {
+    public static int count=0;
+    
+    
+}
